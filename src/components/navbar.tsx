2542a1053e01804f198940e17a7237673109ae6a
Apollo Daniel <developer.apollo.mail@gmail.com>
2025-05-08 18:45:51 -0300

    feat: add dynamic dashboard/auth buttons on navbar depending of session status

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,8 +6,9 @@ import {
 } from "@/components/ui/navigation-menu";
 import Link from "next/link";
 import AppLogo from "./logo";
-import { cn } from "@/lib/utils";
+import { cn, getUserSessionStatus } from "@/lib/utils";
 import { Button } from "./ui/button";
+import { SessionStatus } from "@/lib/types";
 
 type MenuEntry = {
 	label: string;
@@ -30,7 +31,9 @@ const navigationMenuEntries: MenuEntry[] = [
 	{ label: "FAQ", href: "/faq" },
 ];
 
-export default function AppNavbar() {
+export default async function AppNavbar() {
+	const sessionStatus = await getUserSessionStatus();
+
 	return (
 		<div className="fixed top-0 flex w-full flex-row items-center justify-between px-6">
 			<AppLogo />
@@ -49,17 +52,27 @@ export default function AppNavbar() {
 							</Link>
 						</NavigationMenuItem>
 					))}
-					<Link href="/register" passHref className="ms-2">
-						<Button>Register now</Button>
-					</Link>
-					<NavigationMenuItem>
-						<Link
-							href={"/login"}
-							className={cn(navigationMenuTriggerStyle())}
-						>
-							Sign in
-						</Link>
-					</NavigationMenuItem>
+					{sessionStatus == SessionStatus.AUTHENTICATED ? (
+						<>
+							<Link href="/dashboard" passHref className="ms-2">
+								<Button>Dashboard</Button>
+							</Link>
+						</>
+					) : (
+						<>
+							<Link href="/register" passHref className="ms-2">
+								<Button>Register now</Button>
+							</Link>
+							<NavigationMenuItem>
+								<Link
+									href={"/login"}
+									className={cn(navigationMenuTriggerStyle())}
+								>
+									Sign in
+								</Link>
+							</NavigationMenuItem>
+						</>
+					)}
 				</NavigationMenuList>
 			</NavigationMenu>
 		</div>
