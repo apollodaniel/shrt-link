3543d52335e0b401fcc91a53174919abd7a3da7c
Apollo Daniel <developer.apollo.mail@gmail.com>
2025-05-16 14:04:15 -0300

    fix: fix website ico overflowing it's size

--- src/app/dashboard/[id]/page.tsx
+++ src/app/dashboard/[id]/page.tsx
@@ -75,7 +75,7 @@ export default async function UrlDashboard({
 							src={dashboardSummary?.url.metadata!.image}
 							height={24}
 							width={24}
-							className="aspect-square h-[24px] w-[24px] object-cover"
+							className="aspect-square h-[24px] w-[24px] overflow-hidden object-cover"
 						/>
 					)}
 					{dashboardSummary?.url.metadata?.title && (
