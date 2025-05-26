e9438aa81653af09ddfed72c90b9d5baae306dd0
Apollo Daniel <developer.apollo.mail@gmail.com>
2025-05-26 10:43:29 -0300

    revert: re-add rewrites and redirects to next.config while in development

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	/* config options here */
+
+	async redirects() {
+		return [
+			{
+				source: "/api/v1/test/clean",
+
+				destination: `/`,
+
+				permanent: true,
+			},
+		];
+	},
+
+	async rewrites() {
+		return [
+			{
+				source: "/api/v1/:path*",
+
+				destination: `${process.env.API_URL}/api/v1/:path*`,
+			},
+
+			{
+				source: "/:urlid",
+
+				destination: `${process.env.API_URL}/:urlid`,
+			},
+		];
+	},
 	images: {
 		remotePatterns: [
 			{
