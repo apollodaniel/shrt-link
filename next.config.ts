7052f618736f5943ce74a35f7c464c52d4525435
Apollo Daniel <developer.apollo.mail@gmail.com>
2025-06-02 10:00:29 -0300

    chore: remove nextjs rewrites and redirects for production

--- next.config.ts
+++ next.config.ts
@@ -5,33 +5,6 @@ const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
 	/* config options here */
-	async redirects() {
-		return [
-			{
-				source: "/api/v1/test/clean",
-
-				destination: `/`,
-
-				permanent: true,
-			},
-		];
-	},
-
-	async rewrites() {
-		return [
-			{
-				source: "/api/v1/:path*",
-
-				destination: `${process.env.API_URL}/api/v1/:path*`,
-			},
-
-			{
-				source: "/:id([a-zA-Z0-9]{7})",
-
-				destination: `${process.env.API_URL}/:id`,
-			},
-		];
-	},
 	images: {
 		remotePatterns: [
 			{
